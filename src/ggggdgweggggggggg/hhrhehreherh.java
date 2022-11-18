53a5160875fc21ded862340a17fa35b811307237
yeoincheol <yeoincheol@localhost>
2022-11-18 17:32:23 +0900

    Test Commit 추가

--- src/ggggdgweggggggggg/hhrhehreherh.java
+++ src/ggggdgweggggggggg/hhrhehreherh.java
@@ -1,5 +1,8 @@
 package ggggdgweggggggggg;
 
 public class hhrhehreherh {
-
+	public static void main(String[] args) {
+		System.out.println("Hello Worldㅇㅇㅇㅇㅇㅇ");
+		System.out.println("Hello World");
+	}
 }
